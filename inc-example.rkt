a41867645bf178891481f3c0ab2fd2908986d317
Eric Atkinson <eatkinson@mozilla.com>
2015-06-11 17:20:01 -0700

    Implement approximate synthesis with dummy grammar.

--- inc-example.rkt
+++ inc-example.rkt
@@ -207,7 +207,7 @@
     ; the same". This allows us to conclude 
     ; the change must have been instigated by a 
     ; variable not in this set.
-    `(leaf
+    `(
       ; Container dirty-bit
       ,(is-sat? (&& (= width width-new)
                     (= ml ml-new)
@@ -222,7 +222,7 @@
                     (= bottom bottom-new)
                     (= intrins-height intrins-height-new)
                     output-changed))
-      ; Intrinsic dirty-bit
+      ; Inner dirty-bit
       ,(is-sat? (&& (= width width-new)
                     (= ml ml-new)
                     (= mr mr-new)
@@ -238,7 +238,7 @@
                     (= static-y static-y-new)
                     (= container-height container-height-new)
                     output-changed))
-      ; Other dirty-bit
+      ; Intrinsic dirty-bit
       ,(is-sat? (&& (= static-x static-x-new)
                     (= shrink-to-fit shrink-to-fit-new)
                     (= container-width container-width-new)
@@ -252,13 +252,145 @@
 ; Stored as a treemap from auto values to the set of dirty bits to 
 ; be propagated.
 (define (get-precise-db cond-lst)
-  (if (> (length cond-lst) 10)
+  (if (> (length cond-lst) 9)
       (solve-db-constraints cond-lst)
       `(,(get-precise-db (append cond-lst '(#t))) ,(get-precise-db (append cond-lst '(#f)))) 
       ))
 
+(define (synthesize-expression precise)
+  (define-symbolic width-auto boolean?)
+  (define-symbolic ml-auto boolean?)
+  (define-symbolic mr-auto boolean?)
+  (define-symbolic left-auto boolean?)
+  (define-symbolic right-auto boolean?)
+  (define-symbolic height-auto boolean?)
+  (define-symbolic mt-auto boolean?)
+  (define-symbolic mb-auto boolean?)
+  (define-symbolic top-auto boolean?)
+  (define-symbolic bottom-auto boolean?)
+  
+  (define-symbolic container-d boolean?)
+  (define-symbolic inner-d boolean?)
+  (define-symbolic intrins-d boolean?)
+  
+  (define auto-lst (list width-auto ml-auto mr-auto left-auto right-auto height-auto mt-auto mb-auto top-auto bottom-auto))
+  
+
+  
+  
+  
+  (define (soundness-constraint lst precise)
+    (if (= (length lst) 0)
+        (|| (if (car precise) container-d #f) (if (cadr precise) inner-d #f) (if (caddr precise) intrins-d #f))
+        (|| (&& (not (car lst))
+                (soundness-constraint (cdr lst) (cadr precise)))
+            (&& (car lst)
+                (soundness-constraint (cdr lst) (car precise))))))
+  (define sound (soundness-constraint auto-lst precise))
+  sound
+  
+  (define (precision precise approximate)
+    (define count 0)
+    (define (do-count in-lst p)
+      (if (> (length in-lst) 9)
+          (begin
+            (if (|| (&& (car p) (not (approximate #t #f #f in-lst)))
+                    (&& (cadr p) (not (approximate #f #t #f in-lst)))
+                    (&& (caddr p) (not (approximate #f #f #t in-lst))))
+                (begin
+                  (error "Invalid approximation"))
+                #f)
+            (set! count (+ count (if (&& (not (car p)) (approximate #t #f #f in-lst)) 1 0)))
+            (set! count (+ count (if (&& (not (cadr p)) (approximate #f #t #f in-lst)) 1 0)))
+            (set! count (+ count (if (&& (not (caddr p)) (approximate #f #f #t in-lst)) 1 0))))
+          (begin 
+            (do-count (append in-lst '(#t)) (car p))
+            (do-count (append in-lst '(#f)) (cadr p)))))
+    (do-count '() precise)
+    count)
+  
+  (define-synthax (auto-expr k autos)
+    #:assert (> k 0)
+    [choose
+     #t
+     #f
+     (car autos) 
+     (cadr autos) 
+     (caddr autos) 
+     (cadddr autos) 
+     (car (cddddr autos)) 
+     (cadr (cddddr autos)) 
+     (caddr (cddddr autos)) 
+     (cadddr (cddddr autos)) 
+     (car (cddddr (cddddr autos))) 
+     (cadr (cddddr (cddddr autos)))
+     ([choose && ||] (auto-expr (- k 1) autos) (auto-expr (- k 1) autos))
+     ])
+  
+  (define (f db1 db2 db3 autos) (|| (&& (auto-expr 2 autos) db1) (&& (auto-expr 2 autos) db2) (&& (auto-expr 2 autos) db3)))
+  
+  (define (synth f-old bounds)
+    (displayln "synthesizing...")
+
+    (define-symbolic* width-auto-w boolean?)
+    (define-symbolic* ml-auto-w boolean?)
+    (define-symbolic* mr-auto-w boolean?)
+    (define-symbolic* left-auto-w boolean?)
+    (define-symbolic* right-auto-w boolean?)
+    (define-symbolic* height-auto-w boolean?)
+    (define-symbolic* mt-auto-w boolean?)
+    (define-symbolic* mb-auto-w boolean?)
+    (define-symbolic* top-auto-w boolean?)
+    (define-symbolic* bottom-auto-w boolean?)
+
+    (define-symbolic* container-d-w boolean?)
+    (define-symbolic* inner-d-w boolean?)
+    (define-symbolic* intrins-d-w boolean?)
+
+    (define auto-lst-w (list width-auto-w ml-auto-w mr-auto-w left-auto-w right-auto-w height-auto-w mt-auto-w mb-auto-w top-auto-w bottom-auto-w))
+
+    (define m (synthesize 
+                   #:forall (append auto-lst (list container-d inner-d intrins-d))
+                   #:guarantee (assert (&& (implies sound (f container-d inner-d intrins-d auto-lst))
+                                           (f-old container-d-w inner-d-w intrins-d-w auto-lst-w)
+                                           (not (f container-d-w inner-d-w intrins-d-w auto-lst-w))
+                                           bounds))))
+    (display "found expression: ")
+    (displayln (evaluate (f container-d inner-d intrins-d auto-lst) m))
+    (define f-new (lambda (db1 db2 db3 lst) (evaluate (f db1 db2 db3 lst) m)))
+    
+    (display  "computing precision... ")
+    (print (precision precise f-new))
+    (newline)
+
+    (define concrete-witness
+      `(,(evaluate container-d-w m)
+        ,(evaluate inner-d-w m)
+        ,(evaluate intrins-d-w m)
+        (,(evaluate width-auto-w m)
+         ,(evaluate ml-auto-w m)
+         ,(evaluate mr-auto-w m)
+         ,(evaluate left-auto-w m)
+         ,(evaluate right-auto-w m)
+         ,(evaluate height-auto-w m)
+         ,(evaluate mt-auto-w m)
+         ,(evaluate mb-auto-w m)
+         ,(evaluate top-auto-w m)
+         ,(evaluate bottom-auto-w m))))
+
+    (with-handlers ([exn:fail? (lambda (exn) (begin (displayln exn) f-new))])
+      (synth f-new (&& bounds (not (f (car concrete-witness) 
+                                      (cadr concrete-witness)
+                                      (caddr concrete-witness)
+                                      (cadddr concrete-witness))))))
+    )
+  
+  (synth (lambda (db1 db2 db3 lst) #t) #t)
+  )
+
 ; Compute the most accurate dirty-bit function
 (define precise-db (get-precise-db '()))
 
+(define m (synthesize-expression precise-db))
+
 ;(solve-db-constraints '(#f #f #f #f #f #f #f #f #f #f))
-  
